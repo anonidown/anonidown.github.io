8654bdd502fc9c90ab148a547d4965518ea18f0e
anonidown <152479659+anonidown@users.noreply.github.com>
2023-11-30 06:54:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+new Twitch.Embed("twitch-embed", {
+    channel: "vector",
+    height: "100%",
+    layout: "video-with-chat",
+    parent: ["anonidown.github.io"],
+    theme: "dark",
+    width: "100%"});
